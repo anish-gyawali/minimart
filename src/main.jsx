4984fa9f44ba6d1260145c4727d46cf61cec3574
subin <subin.bista@selu.edu>
2025-07-19 10:18:26 -0500

    Created theme context and used  usenavigate hook

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
+  //4. Wraping the App with Provider
   <BrowserRouter>
+  <ThemeProvider>
     <App />
+    </ThemeProvider>
   </BrowserRouter>,
 )
